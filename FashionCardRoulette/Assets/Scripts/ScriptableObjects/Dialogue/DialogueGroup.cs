31c524040106fcce987d5c024a095424f45b7846
Denis Khasanov <denishasanov828@gmail.com>
2025-10-13 12:48:24 +0500

    0.0.1.3.1

--- FashionCardRoulette/Assets/Scripts/ScriptableObjects/Dialogue/DialogueGroup.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DialogueGroup", menuName = "Game/Dialogue/DialogueGroup")]
-public class DialogueGroup : ScriptableObject
-{
-    public List<Dialogue> dialogues = new List<Dialogue>();
-}
