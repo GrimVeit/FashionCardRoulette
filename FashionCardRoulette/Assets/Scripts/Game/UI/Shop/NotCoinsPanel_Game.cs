a4a9600ac1fd55e29eb7f5c3bf075c35b0ca93d2
Denis Khasanov <denishasanov828@gmail.com>
2025-10-28 21:57:05 +0500

    0.0.2.5.1

--- FashionCardRoulette/Assets/Scripts/Game/UI/Shop/NotCoinsPanel_Game.cs
+++ FashionCardRoulette/Assets/Scripts/Game/UI/Shop/NotCoinsPanel_Game.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class NotCoinsPanel_Game : MovePanel
 {
+    [SerializeField] private UIEffectCombination effectCombination;
 
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        effectCombination.Initialize();
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+
+        effectCombination.Dispose();
+    }
+
+    public override void ActivatePanel()
+    {
+        base.ActivatePanel();
+
+        effectCombination.ActivateEffect();
+    }
+
+    public override void DeactivatePanel()
+    {
+        base.DeactivatePanel();
+
+        effectCombination.DeactivateEffect();
+    }
 }
